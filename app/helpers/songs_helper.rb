a965c757a513f103a291158a35edc7aeec416408
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-19 17:33:44 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
-  def artist_select(song, id)
+  def artist_select(song, artist_id)
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
