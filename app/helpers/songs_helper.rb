755a5decd76ad8d294388e32ee3426f46afda7d9
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-19 17:36:32 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -9,6 +9,6 @@ module SongsHelper
   
   
   def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", new_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 end
